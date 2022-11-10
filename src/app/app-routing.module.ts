b28241f90a41065495c36cb6d9410c6e7cf2782f
RobinSartini <robinsartini5@gmail.com>
2022-11-10 17:01:40 +0100

    rfhy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from "@angular/common";
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {ListefichefraisComponent} from "./listefichefrais/listefichefrais.component";
+import {Fichefrais} from "./metier/fichefrais";
+import {AjoutfichefraisComponent} from "./ajoutfichefrais/ajoutfichefrais.component";
+import {NavbarHtmlComponent} from "./navbar/navbar.html.component";
+import {ConnexionComponent} from "./connexion/connexion.component";
+
+const routes: Routes = [
+  {path:'',redirectTo:'/connexion', pathMatch: 'full' },
+  {path:'connexion',component:ConnexionComponent },
+  {path:'ListeficheFrais',component:ListefichefraisComponent },
+  {path: 'modifierFrais/:id', component: Fichefrais},
+  {path: 'ajouterFrais', component:AjoutfichefraisComponent},
+  {path:'accueil', component:NavbarHtmlComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposantRouteurComponent } from './composant-routeur/composant-routeur.component';
+import { ComposantSelecteurComponent } from './composant-selecteur/composant-selecteur.component';
+import { NavbarHtmlComponent } from './navbar/navbar.html.component';
+import { ListefichefraisComponent } from './listefichefrais/listefichefrais.component';
+import { FichefraisComponentComponent } from './fichefrais-component/fichefrais-component.component';
+import { FichefraisComponent } from './fichefrais/fichefrais.component';
+import { AjoutfichefraisComponent } from './ajoutfichefrais/ajoutfichefrais.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposantRouteurComponent,
+    ComposantSelecteurComponent,
+    NavbarHtmlComponent,
+    ListefichefraisComponent,
+    FichefraisComponentComponent,
+    FichefraisComponent,
+    AjoutfichefraisComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
